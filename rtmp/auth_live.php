e13519a492ecfffa94d94364a3a4e9ebc09cc559
Dorkmaster Flek <dorkmasterflek@gmail.com>
2019-01-17 12:23:23 -0500

    Comment on the stream key variable name.

--- rtmp/auth_live.php
+++ rtmp/auth_live.php
@@ -9,6 +9,7 @@ $STREAM_KEYS = array(
     "YOUR_SECRET_STREAM_KEY3"
 );
 
+// The stream key comes through as a POST variable called "name" from the on_publish event in nginx.
 if (in_array($_POST["name"], $STREAM_KEYS)) {
     http_response_code(200);
 } else {
